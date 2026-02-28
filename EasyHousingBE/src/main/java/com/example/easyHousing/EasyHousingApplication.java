4d3efa3f84705546ec3093cd2a4203347684a1c0
Vincenzo <vi200133@gmail.com>
2026-02-28 16:07:14 +0100

    Aggiunto dump del database per la consegna

--- EasyHousingBE/src/main/java/com/example/easyHousing/EasyHousingApplication.java
+++ EasyHousingBE/src/main/java/com/example/easyHousing/EasyHousingApplication.java
@@ -1,13 +1,13 @@
-package com.example.immobiliareClone;
+package com.example.easyhousing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ImmobiliareCloneApplication {
+public class EasyHousingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ImmobiliareCloneApplication.class, args);
+		SpringApplication.run(EasyHousingApplication.class, args);
 	}
 
 }
