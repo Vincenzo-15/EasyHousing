5645a7ba325b394f8b1cfe9b9d7974b051deb1b0
Vincenzo <vi200133@gmail.com>
2026-03-28 11:39:31 +0100

    aggiunta grafici

---
+++ EasyHousingFE/src/app/components/footer/footer.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent {
+  annoCorrente = new Date().getFullYear();
+
+  mostraAvviso(event: Event) {
+    event.preventDefault(); // Blocca lo scatto della pagina verso l'alto
+    alert('Questa pagina è un Mockup grafico (solo a scopo dimostrativo per l\'esame) e non è implementata nel backend.');
+  }
+}
+
+
